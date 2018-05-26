c2809be79f27eec4aa9d15206947e1ac8323727c
Pratyush Yadav <pratyush.mitma1@learner.manipal.edu>
2018-05-26 11:41:09 +0530

    Removed extra indentation from xen_bus_dma_tag_create() prototype
    Make it match with the indentation in the other methods.

--- sys/xen/gnttab.h
+++ sys/xen/gnttab.h
@@ -125,10 +125,10 @@ int gnttab_resume(device_t);
  */
 
 int xen_bus_dma_tag_create(bus_dma_tag_t parent, bus_size_t alignment,
- 	 		bus_addr_t boundary, bus_addr_t lowaddr, bus_addr_t highaddr,
- 	 		bus_dma_filter_t *filtfunc, void *filtfuncarg,	bus_size_t maxsize,
- 	 		int nsegments,	bus_size_t maxsegsz, int flags,
- 	 		bus_dma_lock_t	*lockfunc, void	*lockfuncarg, bus_dma_tag_t *dmat);
+ 	 	bus_addr_t boundary, bus_addr_t lowaddr, bus_addr_t highaddr,
+ 	 	bus_dma_filter_t *filtfunc, void *filtfuncarg,	bus_size_t maxsize,
+ 	 	int nsegments,	bus_size_t maxsegsz, int flags,
+ 	 	bus_dma_lock_t	*lockfunc, void	*lockfuncarg, bus_dma_tag_t *dmat);
 
 int xen_bus_dma_tag_destroy(bus_dma_tag_t dmat, grant_ref_t *refs,
 		unsigned int refcount);
